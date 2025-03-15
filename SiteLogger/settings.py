b125a7fe968b7163ad8dc869181b15a015f7bf2b
SaliMerc <nyawadesaline@gmail.com>
2025-03-15 13:52:36 +0300

    gitignore config

--- SiteLogger/settings.py
+++ SiteLogger/settings.py
@@ -33,7 +33,7 @@ SECRET_KEY = 'django-insecure-*@#xrccg5(2b&fjog^)eh-kpxrex3vlqlza*)=f1xij4t6n7&w
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 CSRF_TRUSTED_ORIGINS = [
-    '127.0.0.1',
+    'http://127.0.0.1',
     'https://sitemngmtapp-4.onrender.com',
 ]
 ALLOWED_HOSTS = [
