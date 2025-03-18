cea5f53909965904a07e0c5009bf6fa0418d4662
SaliMerc <nyawadesaline@gmail.com>
2025-03-18 22:58:38 +0300

    new dashboard look

--- staticfiles/js/script.js
+++
@@ -1,6 +0,0 @@
-$(window).scroll(function() {
-    const scroll = $(window).scrollTop();
-    if (scroll > 50) {
-        $(".navbar").css("background-color", "#435a42");
-    } else {
-        $(".navbar").css("background-color", "transparent"); } });
